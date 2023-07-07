c81a8470efe57b59de0676ad8b3fcbc942908915
vinicius webster <viniciuswebster99@gmail.com>
2023-07-06 21:18:35 -0300

    ajustando reponsividade do card de informaçoes

--- src/components/Infos/index.js
+++ src/components/Infos/index.js
@@ -9,21 +9,23 @@ function CardInfos({titulo, subtitulo}) {
     ]
 
     return(
-        <div className='card__infos container'>
-            <img src='assets/planta-como-conseguir.png' />           
+    <section className='container'>
+        <div className='card__infos'>
+            <img src='assets/planta-como-conseguir.png' alt='planta'/>           
             <div className='card__infos-infos'>
                 <h4>{subtitulo}</h4>
                 <h3>{titulo}</h3>
                 <ul>
-                    {lista.map(item =>
+                    {lista.map((item, index) =>
                         <div className='info'> 
                             <img src='assets/Ellipse.svg' alt='Elipse'/>
-                            <li>{item}</li>                   
+                            <li key={index}>{item}</li>                   
                         </div>
                     )}
                 </ul>
             </div>
         </div>
+    </section>
     )
 }
 
