d0a681de51a634d54aa04697e30e6fd2ff72fb4a
vinicius webster <viniciuswebster99@gmail.com>
2023-07-06 21:19:48 -0300

    ajustando responsividade da seção ofertas

--- src/components/Ofertas/index.js
+++ src/components/Ofertas/index.js
@@ -1,3 +1,4 @@
+import Cards from '../Cards';
 import './Ofertas.css';
 
 function Ofertas({cards}) {
@@ -5,9 +6,8 @@ function Ofertas({cards}) {
     <section className='ofertas container'>
         <h3>Conheça nossas</h3>
         <h2>ofertas</h2>
-
-        <ul>
-            {/* {cards.map(card => <li>{card}</li>)} */}
+        <ul className='produtos'>
+            {cards.map(card => <Cards key={card.nome} nome={card.nome} preco={card.preco} imagem={card.imagem} />)}
         </ul>
     </section>
     )
