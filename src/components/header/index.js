b4675b3bad57988ef9d1fa46e417f5038b35c2ec
vinicius webster <viniciuswebster99@gmail.com>
2023-07-07 20:59:31 -0300

    melhorando responsividade

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,19 @@
 import './Header.css';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 function Header() {
     return(
         <header className='header container'>
         <a href='#logo'><img src='assets/logo.svg' alt='Logo Casa Verde'/> </a>  
             <nav>
-                <ul className='nav'>
+            <RxHamburgerMenu  className='menu__hamburger' size={40}/>
+                <ul className='nav'>         
                     <li key={1}><a href='#comofaver'>Como fazer </a></li> /
                     <li key={2}><a href='#ofertas'>Ofertas</a></li> /
                     <li key={3}><a href='#depoimentos'> Depoimentos</a></li> /
                     <li key={4}><a href='#videos'>Videos</a></li> /
-                    <li key={5}><a href='#meu carrinho'>Meu carrinho</a></li>
+                    <li> <AiOutlineShoppingCart  size={25}/></li>
                 </ul>
             </nav>
         </header>
