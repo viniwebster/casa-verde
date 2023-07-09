a72012a61a98aa992b9f0b6bcd2092033bd6f38b
vinicius webster <viniciuswebster99@gmail.com>
2023-07-09 20:20:35 -0300

    melhorias no carrinho e melhorando versao mobile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,24 +1,53 @@
 import './Header.css';
 import { RxHamburgerMenu } from 'react-icons/rx';
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 import Cart from '../Cart';
 
 function Header({compras, deleteItem}) {
+
+    function openCart() {
+        const cart = document.querySelector('.cart');
+            if (cart.style.display === 'none') {
+                cart.style.display = 'flex'
+            } else {
+                cart.style.display = 'none'
+            }  
+
+        const menu = document.querySelector('.nav');
+        if (menu.style.display === 'flex') {
+            menu.style.display = 'none'
+        }
+    }
+
+    function openMenuMobile() {
+        const menu = document.querySelector('.nav');
+            if (menu.style.display === 'none') {
+                menu.style.display = 'flex'
+            } else {
+                menu.style.display = 'none'
+            }  
+
+            const cart = document.querySelector('.cart');
+            if (cart.style.display === 'flex') {
+                cart.style.display = 'none'
+            }
+    }
+
     return(
         <header className='header container'>
         <a href='#logo'><img src='assets/logo.svg' alt='Logo Casa Verde'/> </a>  
-            <nav>
-            <RxHamburgerMenu className='menu__hamburger' size={40}/>
+            <nav className='nav-header'>
+            <RxHamburgerMenu className='menu__hamburger' onClick={() => openMenuMobile()} size={40}/>
                 <ul className='nav'>         
                     <li key={1}><a href='#comofaver'>Como fazer </a></li>
                     <li key={2}><a href='#ofertas'>Ofertas</a></li>
                     <li key={3}><a href='#depoimentos'> Depoimentos</a></li>
                     <li key={4}><a href='#videos'>Videos</a></li>
-                    <li className='carrinho'>
-                        <AiOutlineShoppingCart  size={25}/>
-                        <Cart compras={compras} deleteItem={deleteItem}/>             
-                    </li>
                 </ul>
+                <div className='carrinho'>
+                        <AiOutlineShoppingCart className='cart-icon' size={25} onClick={ () => openCart()}/>
+                        <Cart compras={compras}  deleteItem={deleteItem} />             
+                </div>
             </nav>
         </header>
     )
